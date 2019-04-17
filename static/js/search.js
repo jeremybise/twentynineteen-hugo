82cff7acefe14a7175301bfc934bfb87429fbf3e
Jeremy Bise <jeremy.bise@thosegeeks.com>
2019-04-17 10:34:37 -0400

    format no matches

--- static/js/search.js
+++ static/js/search.js
@@ -33,7 +33,9 @@ function executeSearch(searchQuery) {
     if (result.length > 0) {
       populateResults(result);
     } else {
-      $("#search-results").append("<p>No matches found</p>");
+      $("#search-results").append(
+        "<article class='post entry'><div class='entry-content'>No matches found</div></article>"
+      );
     }
   });
 }
